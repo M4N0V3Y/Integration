32f3a2ff51c0a70706add1317477965659f756fc
M4N0V3Y <barbosajaf@gmail.com>
2018-11-19 11:59:50 -0200

    Service Integration
    Service Integration

---
+++ IntegracaoPsP.Service/PsP.asmx.cs
@@ -0,0 +1,101 @@
+﻿using IntegracaoPsP.Factory.Interfaces;
+using IntegracaoPsP.Factory.Log;
+using IntegracaoPsP.Infraestrutura.Container;
+using IntegracaoPsP.Infraestrutura.Dominio.Entidades.Outros;
+using IntegracaoPsP.Infraestrutura.Dominio.Enum;
+using IntegracaoPsP.Infraestrutura.Proxy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Services;
+using Unity;
+
+namespace IntegracaoPsP.Service
+{
+    /// <summary>
+    /// Summary description for PsP
+    /// </summary>
+    [WebService(Namespace = "http://linkcon.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class PsP : ServiceBase
+    {
+        #region Propriedades
+        private FtpProxy Proxy;
+        private Logger Logger;
+        private IGerenciador<Parametro> GerenciadorPrametro;
+        #endregion
+
+        #region WebMethods
+        [WebMethod(Description = @"Método responsável em verificar se existem arquivos a serem processados")]
+        public string BuscarArquivos()
+        {
+            OnStart();
+            Proxy = new FtpProxy();
+            try
+            {
+                return Proxy.PercorrerDiretorio(null);
+            }
+            catch (Exception e)
+            {
+                Logger = new Logger(e.ToString(), EnumStatusLog.Nok, DateTime.Now);
+                Logger.WriteLog();
+                // return "NOK"; 
+                return string.Format(" [ NOK ] " + e.ToString());
+            }
+        }
+
+        [WebMethod(Description = @"Método responsável processar um arquivo de fomra individual")]
+        public string BuscarArquivosIndividual(string caminho)
+        {
+            OnStart();
+            Proxy = new FtpProxy();
+            try
+            {
+                return Proxy.PercorrerDiretorio(caminho);
+            }
+            catch (Exception e)
+            {
+                Logger = new Logger(e.ToString(), EnumStatusLog.Nok, DateTime.Now);
+                Logger.WriteLog();
+                return "NOK";
+            }
+        }
+
+        [WebMethod(Description = @"Método que retorna o valor de um determinado parametro")]
+        public string BuscarParametro(string nomeParametro)
+        {
+            OnStart();
+            try
+            {
+                GerenciadorPrametro = DependencyContainer.Container.Resolve<IGerenciador<Parametro>>();
+                return ((Parametro)GerenciadorPrametro.Gerenciar(new Parametro() { Nome = nomeParametro }, EnumOperacao.SelecionarPorNome)).Valor;
+            }
+            catch (Exception e)
+            {
+                Logger = new Logger(e.ToString(), EnumStatusLog.Nok, DateTime.Now);
+                Logger.WriteLog();
+                return "NOK";
+            }
+        }
+
+        [WebMethod(Description = @"Método de teste do webservice")]
+        public string TestarConexaoWS()
+        {
+            try
+            {
+                return "OK";
+            }
+            catch (Exception e)
+            {
+                Logger = new Logger(e.ToString(), EnumStatusLog.Nok, DateTime.Now);
+                Logger.WriteLog();
+                return "NOK";
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ IntegracaoPsP.Service/ServiceBase.cs
@@ -0,0 +1,46 @@
+﻿using IntegracaoPsP.Factory.Biblioteca;
+using IntegracaoPsP.Factory.Gerenciador;
+using IntegracaoPsP.Factory.Interfaces;
+using IntegracaoPsP.Factory.Processador;
+using IntegracaoPsP.Infraestrutura.Container;
+using IntegracaoPsP.Infraestrutura.Dominio.Entidades.Log;
+using IntegracaoPsP.Infraestrutura.Dominio.Entidades.Outros;
+using IntegracaoPsP.Infraestrutura.Dominio.Entidades.WS;
+using IntegracaoPsP.Infraestrutura.Repositorio.Base;
+using IntegracaoPsP.Infraestrutura.Repositorio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Unity;
+
+namespace IntegracaoPsP.Service
+{
+    public class ServiceBase : WebService
+    {
+        public void OnStart()
+        {
+            if (DependencyContainer.Container == null)
+            {
+                DependencyContainer.Container = new UnityContainer();
+
+                DependencyContainer.Container.RegisterType(typeof(IGerenciador<>), typeof(GerenciadorPadrao<>));
+                DependencyContainer.Container.RegisterType(typeof(IProcessador<>), typeof(ProcessadorPadrao<>));
+
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<LogArquivo>), typeof(LogArquivoBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<LogIntegracao>), typeof(LogIntegracaoBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<LogMessage>), typeof(LogMessageBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<ModeloXsd>), typeof(ModeloXsdBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<Parametro>), typeof(ParametroBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<WS_LISTA>), typeof(WsListaBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<WS_TIPOS>), typeof(WsTipoBll));
+                DependencyContainer.Container.RegisterType(typeof(IBiblioteca<Log>), typeof(LogBll));
+
+                //repositorios
+                DependencyContainer.Container.RegisterType(typeof(IRepositorio<>), typeof(RepositorioBase<>));
+                DependencyContainer.Container.RegisterType(typeof(IRepositorio<>), typeof(RepositorioBaseS2GPI<>));
+            }
+        }
+    }
+}
